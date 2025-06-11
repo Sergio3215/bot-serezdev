c3ad194221f59784d591d4f0e39dc192652db609
Sergio <principiante.en.programar@gmail.com>
2025-06-11 05:39:16 -0300

    fix issue with export

--- openaiScript.js
+++ openaiScript.js
@@ -1,27 +1,28 @@
-const OpenAI =  require("openai");
+const OpenAI = require("openai");
 require("dotenv").config;
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export const ConsultingOpenAI = async (prompt) => {
+const ConsultingOpenAI = async (prompt) => {
     const response = await openai.chat.completions.create({
         model: "gpt-4o",
-        messages: [{ role: "user", content: `
-La siguiente consulta debe responderse en un máximo de 1900 caracteres.
-- Sé directo, omite relleno.
-- Resumi cualquier respuesta en algo concreto.
-- Usa un lenguaje sencillo.
-- No repitas ideas.
-- Usa frases cortas.
-- No uses listas largas.
-Consulta: ${prompt}
-` }],
+        messages: [{
+            role: "user", content: `
+                    La siguiente consulta debe responderse en un máximo de 1900 caracteres.
+                    - Sé directo, omite relleno.
+                    - Resumi cualquier respuesta en algo concreto.
+                    - Usa un lenguaje sencillo.
+                    - No repitas ideas.
+                    - Usa frases cortas.
+                    - No uses listas largas.
+                    Consulta: ${prompt}
+        ` }],
     });
 
     const reply = response.choices[0].message.content;
     return reply;
-} 
+}
 
-// module.exports = {
-//     ConsultingOpenAI
-// };
+module.exports = {
+    ConsultingOpenAI
+};
