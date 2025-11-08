03de8f002974d42af13f32cd5047bb36f980c975
Sergio <principiante.en.programar@gmail.com>
2025-11-08 07:25:47 -0300

    fix command in alert of streaming x2

--- commands/rules.js
+++ commands/rules.js
@@ -69,7 +69,7 @@ const Rules = async (msg) => {
         }
     }
 
-    if (msg.channel.name.includes('anuncio') || msg.channel.name.includes('stream')) {
+    if (msg.channel.name.includes('anuncio') || msg.channel.name.includes('stream') || msg.channel.name.includes('twitch')|| msg.channel.name.includes('tiktok')|| msg.channel.name.includes('kick')) {
         return;
     }
 
