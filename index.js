97a4bca51615dfdff55a1415585ecf4fd182e4ea
Sergio <principiante.en.programar@gmail.com>
2026-01-25 02:16:54 -0300

    time run bd

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ client.on('ready', () => {
 
     let date = new Date();
 
-    if (date.getHours() == 5 && date.getMinutes() == 15) {
+    if (date.getHours() == 2 && date.getMinutes() == 20) {
         library.birthday_runtime(client);
     }
 
